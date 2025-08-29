6d9d93e55d3bbc2af9e2421c15a249c232cf562c
ChaitanyaKrishna2004 <paruchurichaitanyakrishna6@gmail.com>
2025-08-29 18:34:35 +0530

    ep - 13

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,9 +1,46 @@
 import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
-test("should load contact us component", () => {
-  render(<Contact />);
-  const heading = screen.getByRole("heading");
 
-  expect(heading).toBeInTheDocument();
+describe("Contact Us Page Test Case", () => {
+  it("should load contact us component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("should load button inside contact component", () => {
+    render(<Contact />);
+    // const button = screen.getByRole("button");
+    const button = screen.getByText("Submit");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should load input name inside contact component", () => {
+    render(<Contact />);
+
+    const inputname = screen.getByPlaceholderText("name");
+
+    expect(inputname).toBeInTheDocument();
+  });
+
+  it("should load 2 input name inside contact component", () => {
+    render(<Contact />);
+
+    //Querying
+    const inputBoxes = screen.getAllByRole("textbox");
+    // console.log(inputBoxes.length);
+    //Assertion
+    expect(inputBoxes.length).toBe(2);
+  });
+
+  it("should load 2 input name inside contact component", () => {
+    render(<Contact />);
+    //Querying
+    const inputBoxes = screen.getAllByRole("textbox");
+    //Assertion
+    expect(inputBoxes.length).not.toBe(3);
+  });
 });
